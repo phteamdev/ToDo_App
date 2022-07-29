0df9fb849e74abbb783c7535e70865483dd6b6b4
phteamdev <107764369+phteamdev@users.noreply.github.com>
2022-07-29 10:54:58 +0430

    Add files via upload

---
+++ constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color lightblue = HexColor('7BD4CC');
+final Color Honey = HexColor('BEA42E');
+final Color Army = HexColor('7B895B');
+final Color DarkBlue = HexColor('037367');
+final Color Sangira = HexColor('00281F');
